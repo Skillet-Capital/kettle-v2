358b2eaa9eae66fb03367a6ed2e3b55ef0e1e8ec
Andrew Warner <warneracw@gmail.com>
2024-10-29 17:31:57 -0400

    export multicall methods

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from "./Kettle";
 export * from "./types";
+export * from "./multicall";
---
+++ src/multicall/index.ts
@@ -0,0 +1,4 @@
+export * from "./availability";
+export * from "./collaterals";
+export * from "./terms";
+export * from "./validateMarketOffers";
