ff6267b29116f999a1a8fdff5b894aec2bfba73a
Andrew Warner <warneracw@gmail.com>
2024-10-30 13:49:04 -0400

    index liens (#11)

--- handlers/helpers.ts
+++ handlers/helpers.ts
@@ -3,3 +3,7 @@ import { Address, BigInt, ethereum } from "@graphprotocol/graph-ts"
 export function formatCollateralId(collection: Address, tokenId: BigInt): string {
   return [collection.toHexString(), tokenId.toString()].join("/");
 }
+
+export function formatLienId(address: Address, lienId: BigInt): string {
+  return [address.toHexString(), lienId.toString()].join("/");
+}
