7eb42fbf30849604fa78dc65c5739d4910311de3
Andrew Warner <warneracw@gmail.com>
2024-12-05 11:02:21 -0500

    update deployment scripts

--- ignition/modules/Kettle.ts
+++ ignition/modules/Kettle.ts
@@ -1,81 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const LockModule = buildModule("Kettle", (m) => {
+const KettleModule = buildModule("Kettle", (m) => {
   const owner = m.getAccount(0);
-
-  // Deploy Lender Receipt
-  const receipt = m.contract("LenderReceipt");
-
-  // Deploy Lending Controller Implementation
-  const lendingImplementation = m.contract("LendingController", [], { id: "LendingControllerImplementation" });
-
-  // Deploy Lending Controller Proxy
-  const lendingProxy = m.contract("TransparentUpgradeableProxy", [
-    lendingImplementation,
-    owner,
-    "0x"
-  ], { id: "LendingControllerProxy" });
-
-  const lendingProxyAdminAddress = m.readEventArgument(
-    lendingProxy,
-    "AdminChanged",
-    "newAdmin",
-    { id: "LendingControllerProxyAdminChanged" }
-  );
-
-  const lendingProxyAdmin = m.contractAt("ProxyAdmin", lendingProxyAdminAddress, { id: "LendingControllerProxyAdmin" });
-  
-  const lending = m.contractAt("LendingController", lendingProxy, { id: "LendingController" });
-
-  m.call(lending, "__LendingController_init", [owner, receipt]);
-
-  m.call(receipt, "setSupplier", [lending, 1]);
-
-  // Deploy Escrow Controller Implementation
-  const escrowImplementation = m.contract("EscrowController", [], { id: "EscrowControllerImplementation" });
-
-  // Deploy Escrow Controller Proxy
-  const escrowProxy = m.contract("TransparentUpgradeableProxy", [
-    escrowImplementation,
-    owner,
-    "0x",
-  ], { id: "EscrowControllerProxy" });
-
-  const escrowProxyAdminAddress = m.readEventArgument(
-    escrowProxy,
-    "AdminChanged",
-    "newAdmin",
-    { id: "EscrowControllerProxyAdminChanged" }
-  );
-
-  const escrowProxyAdmin = m.contractAt("ProxyAdmin", escrowProxyAdminAddress, { id: "EscrowControllerProxyAdmin" });
-
-  const escrow = m.contractAt("EscrowController", escrowProxy, { id: "EscrowController" });
-
-  m.call(escrow, "__EscrowController_init", [owner]);
-
-  // Deploy Redemption Controller Implementation
-  const redemptionImplementation = m.contract("RedemptionController", [], { id: "RedemptionControllerImplementation" });
-
-  // Deploy Escrow Controller Proxy
-  const redemptionProxy = m.contract("TransparentUpgradeableProxy", [
-    redemptionImplementation,
-    owner,
-    "0x",
-  ], { id: "RedemptionControllerProxy" });
-
-  const redemptionProxyAdminAddress = m.readEventArgument(
-    redemptionProxy,
-    "AdminChanged",
-    "newAdmin",
-    { id: "RedemptionControllerProxyAdminChanged" }
-  );
-
-  const redemptionProxyAdmin = m.contractAt("ProxyAdmin", redemptionProxyAdminAddress, { id: "RedemptionControllerProxyAdmin" });
-
-  const redemption = m.contractAt("RedemptionController", redemptionProxy, { id: "RedemptionController" });
-
-  m.call(redemption, "__RedemptionController_init", [owner, owner, owner]);
 
   // Deploy Kettle Implementation
   const kettleImplementation = m.contract("Kettle", [], { id: "KettleImplementation" });
@@ -98,22 +24,21 @@ const LockModule = buildModule("Kettle", (m) => {
 
   const kettle = m.contractAt("Kettle", kettleProxy, { id: "Kettle" });
 
-  m.call(kettle, "__Kettle_init", [owner, lending, escrow, redemption]);
+  m.call(kettle, "__Kettle_init", [owner]);
 
-  m.call(lending, "setKettle", [kettle]);
-  m.call(escrow, "setKettle", [kettle]);
+  const TOKEN_SUPPLIER = "0xd50e88321Ac15643C25A23C784eD850A97527580";
+  const REDEMPTION_ADMIN = "0x00BDaaDb9DdF3800703725A71c97069495915191";
+  const REDEMPTION_WALLET = "0x0cFe2D2beEe2fA2D653d1Dd253231648794dD2a8";
+
+  m.call(kettle, "setTokenSupplier", [TOKEN_SUPPLIER]);
+  m.call(kettle, "setRedemptionAdmin", [REDEMPTION_ADMIN]);
+  m.call(kettle, "setRedemptionWallet", [REDEMPTION_WALLET]);
+  m.call(kettle, "setRedemptionFeeCollector", [REDEMPTION_WALLET]);
 
   return { 
-    receipt,
-    lending, 
-    escrow, 
-    redemption,
     kettle, 
-    lendingProxyAdmin, 
-    escrowProxyAdmin, 
-    redemptionProxyAdmin,
     kettleProxyAdmin 
   };
 });
 
-export default LockModule;
+export default KettleModule;
