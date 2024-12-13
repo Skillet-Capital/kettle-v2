a6178928d34221d8366e8ed75b05ef13e22adf62
Andrew Warner <warneracw@gmail.com>
2024-12-12 19:00:53 -0500

    whitelist sellers

--- ignition/modules/DEV/WhitelistedSellers.ts
+++ ignition/modules/DEV/WhitelistedSellers.ts
@@ -1,11 +1,12 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-import KettleModule from "./KettleDev";
+import KettleModule from "./Kettle";
 
 const WhitelistSellers = buildModule("whitelist_sellers_dev", (m) => {
   const { kettle } = m.useModule(KettleModule);
 
   m.call(kettle, "whitelistAskMaker", ["0x11D351894506e13587D4e479b6c38E68891f1492", true], { id: "whitelist_me_1" });
   m.call(kettle, "whitelistAskMaker", ["0x2eefe055e429E51Fd5658b05F4Bd88b53434958B", true], { id: "whitelist_me_2" });
+  m.call(kettle, "whitelistAskMaker", ["0x1D75d322906E998b3a34a93f1D69d1f0bDb9e0cC", true], { id: "whitelist_me_3" });
 
   return { kettle };
 });
