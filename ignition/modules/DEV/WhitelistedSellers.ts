75c87f3bc780e69da303c6f4044bf11e9054e76e
Andrew Warner <warneracw@gmail.com>
2024-12-17 12:54:41 -0500

    whitelist billy

--- ignition/modules/DEV/WhitelistedSellers.ts
+++ ignition/modules/DEV/WhitelistedSellers.ts
@@ -8,6 +8,7 @@ const WhitelistSellers = buildModule("whitelist_sellers_dev", (m) => {
   m.call(kettle, "whitelistAskMaker", ["0x2eefe055e429E51Fd5658b05F4Bd88b53434958B", true], { id: "whitelist_me_2" });
   m.call(kettle, "whitelistAskMaker", ["0x1D75d322906E998b3a34a93f1D69d1f0bDb9e0cC", true], { id: "whitelist_me_3" });
   m.call(kettle, "whitelistAskMaker", ["0x940C2d8c0420Ef01BF8cc8e9d053C0E3Ee35449b", true], { id: "whitelist_me_4" });
+  m.call(kettle, "whitelistAskMaker", ["0x3ebabb4da024d3346e0b42fc16c6e64fc5212870", true], { id: "whitelist_me_5" });
 
   return { kettle };
 });
